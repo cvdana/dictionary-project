537183146c2af4e34239de7f2f4e12ae3f48fd82
cvdana <cvdana99@gmail.com>
2023-08-02 12:16:15 +0200

    set up search enginee

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            <a
+              href="https://github.com/cvdana/dictionary-project"
+              target="blank"
+              rel="noreferrer"
+            >
+              Open-source project
+            </a>{" "}
+            coded by Dana Visnitchi
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
