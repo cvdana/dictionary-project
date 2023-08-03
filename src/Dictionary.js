8dc74515449dafca3cdc46ce94c5d3619f7db532
cvdana <cvdana99@gmail.com>
2023-08-03 15:35:16 +0200

    integrated api dictionary

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState(``);
+  function handleResponse(response) {
+    console.log(response.data);
+  }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
+    const apiKey = `94413t4dbc141o4dc71ce00caf84f31e`;
+    const apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function updateKeyword(event) {
     setKeyword(event.target.value);
